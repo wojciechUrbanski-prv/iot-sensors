0264b7961cad78d1b7f1d263737d70dca7dca907
wojciechUrbanski-prv <wojteku1@wp.pl>
2020-08-02 17:42:50 +0200

    Protection against NPE if given directory does not exists

--- src/main/scala/wojciech/urbanski/statistics/ProcessFiles.scala
+++ src/main/scala/wojciech/urbanski/statistics/ProcessFiles.scala
@@ -9,7 +9,7 @@ import cats.syntax.apply._
 import cats.syntax.flatMap._
 import cats.syntax.functor._
 import fs2.io.file
-import fs2.{Stream, text}
+import fs2.{text, Stream}
 import io.chrisdavenport.log4cats.slf4j.Slf4jLogger
 import io.chrisdavenport.log4cats.{Logger, SelfAwareStructuredLogger}
 import wojciech.urbanski.sensordata.{SensorData, SensorFileData}
@@ -22,7 +22,14 @@ class ProcessFiles[F[_]: ContextShift: Sync: Monad] {
 
   def readFromFilesAndGatherStatistics(path: Path): F[OverallStatistics] = {
     for {
-      files <- Sync[F].delay(new File(path.toString).listFiles().filter(_.isFile).filter(_.getName.endsWith(".csv")).filter(_.getName != "celsius.csv").map(file => Paths.get(file.getPath)).toList)
+      files <- Sync[F].delay(
+                 Option(new File(path.toString).listFiles())
+                   .getOrElse(Array.empty)
+                   .filter(_.isFile)
+                   .filter(_.getName.endsWith(".csv"))
+                   .map(file => Paths.get(file.getPath))
+                   .toList
+               )
       res <- if (files.isEmpty) Logger[F].info("Could not find any .csv files in provided location") *> Monad[F].point(OverallStatistics.empty)
              else readFiles(files).compile.fold(OverallStatistics.empty)(gatherData)
     } yield res
@@ -51,7 +58,7 @@ class ProcessFiles[F[_]: ContextShift: Sync: Monad] {
   }
 
   private def gatherData(acc: OverallStatistics, dataWithPath: (SensorData, Path)) =
-      acc.addNewSensorData(dataWithPath._2, dataWithPath._1)
+    acc.addNewSensorData(dataWithPath._2, dataWithPath._1)
 
   implicit class StreamWithIndexOps[O](stream: Stream[F, (O, Long)]) {
 
